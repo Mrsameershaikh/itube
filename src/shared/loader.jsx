3a535adf3a0071f48a80a63f7de0a664d0682965
Sameer Shaikh <98098376+Mrsameershaikh@users.noreply.github.com>
2023-09-28 16:07:35 +0530

    itube
    itube video app first commit

---
+++ src/shared/loader.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Loader = () =>{
+    return (
+        <div className='load-bar'>
+              <div className="bar"></div>
+              <div className="bar"></div>
+              <div className="bar"></div>   
+        </div>
+    )
+}
